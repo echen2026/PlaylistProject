e2a6e544fe8d81bf7b267fa3151201f0de34c0cf
Warren Peng <wpeng26@students.polytechnic.org>
2024-01-17 10:09:08 -0800

    made playlist in tester

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -10,6 +10,7 @@ public class PlaylistTester {
     public static void main(String[] args) {
         System.out.println("Initializing a Playlist...\n");
         //Make your playlist here
+        Playlist yunomilesbesthits = new Playlist();
 
         System.out.println("Adding songs to the Playlist...\n");
         /**
@@ -18,7 +19,7 @@ public class PlaylistTester {
          * p.addSong(new Song(..., ..., ...))
          */
 
-
+        yunomilesbesthits.addSong()
 
         System.out.println("Printing the songs...\n");
         //Print out all the songs in the playlist to verify it's working correctly
