3cd8b31ef3381c10a3f72415d7a4782ccab54645
Warren Peng <wpeng26@students.polytechnic.org>
2024-01-18 11:43:42 -0800

    changed time

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -13,7 +13,7 @@ public class PlaylistTester {
         Playlist yunomilesbesthits = new Playlist();
 
         System.out.println("Adding songs to the Playlist...\n");
-        Song indianajones = new Song("Indian Jones", "Yuno Miles", 63);
+        Song indianajones = new Song("Indian Jones", "Yuno Miles", 64);
         Song fourwheeler = new Song("4 Wheeler", "Yuno Miles", 106);
         Song firstdayofchristmas = new Song("First Day of Christmas", "Yuno Miles", 121);
         Song spiderpig = new Song("Spider Pig", "Hans Zimmer", 64);
